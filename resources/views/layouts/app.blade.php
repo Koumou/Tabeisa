0afc8b3d437728f12068c12e0b34351c65e94fc2
Koumou <kesselokinga@gmail.com>
2021-10-25 14:35:08 +0200

    fix bugs

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,7 +25,8 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
-    <link href="{{ asset('css/tabeisa.css') }}" rel="stylesheet">
+    <link href="/css/tabeisa.css" rel="stylesheet">
+
 
 </head>
 
